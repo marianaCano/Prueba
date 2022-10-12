ef82921539c5a6ad0b250c5560f68f7ed5825bda
marianaCano <60165489+marianaCano@users.noreply.github.com>
2022-10-12 15:51:16 -0500

    registro exitoso

---
+++ app.js
@@ -0,0 +1,26 @@
+function singUp(e){
+    event.preventDefault();
+
+    var username = document.getElementById("usuario").value;
+    var pass = document.getElementById("contraseña").value;
+
+    var user = {
+        usuario: username,
+        contraseña: pass,
+    };
+    
+    var json = JSON.stringify(user);
+    localStorage.setItem(username, json)
+    console.log ("registrado")
+}
+
+function loginFunc(e){
+    event.preventDefault();
+    
+    var username = document.getElementById("usuario").value;
+    var pass = document.getElementById("Contraseña").value;
+    var result = document.getElementById("result").value;
+    var user = localStorage.getItem(username);
+    var data = JSON.parse(user);
+    console.log(data);
+}
